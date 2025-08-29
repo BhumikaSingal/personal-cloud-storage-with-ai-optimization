f27ccb9ef621b0a547b0eaf1a3b76dc34db7a1bb
BhumikaSingal <bhumika23456@gmail.com>
2025-08-29 21:53:05 +0530

    Create App.js

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import CodeEditor from './Editor';
+
+function App() {
+  return (
+    <div>
+      <h2>React + Monaco Editor</h2>
+      <CodeEditor />
+    </div>
+  );
+}
+
+export default App;
